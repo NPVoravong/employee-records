8317aadff1c17f092b4bf4a1a48d4961b2952724
Nick Voravong <npvoravong@gmail.com>
2020-11-09 10:38:11 -0500

    Final Schema SQL

---
+++ EmployeeSQL/pewit_hackard_schema.sql
@@ -0,0 +1,71 @@
+﻿CREATE TABLE "dept_manager" (
+    "dept_num" VARCHAR   NOT NULL,
+    "emp_num" INT   NOT NULL,
+    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
+        "dept_num","emp_num"
+     )
+);
+
+CREATE TABLE "dept_emp" (
+    "emp_num" INT   NOT NULL,
+    "dept_num" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
+        "emp_num","dept_num"
+     )
+);
+
+CREATE TABLE "employees" (
+    "emp_num" INT   NOT NULL,
+    "emp_title_id" VARCHAR   NOT NULL,
+    "birth_date" VARCHAR   NOT NULL,
+    "first_name" VARCHAR   NOT NULL,
+    "last_name" VARCHAR   NOT NULL,
+    "gender" VARCHAR   NOT NULL,
+    "hire_date" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_num"
+     )
+);
+
+CREATE TABLE "departments" (
+    "dept_num" VARCHAR   NOT NULL,
+    "dept_name" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_num"
+     )
+);
+
+CREATE TABLE "titles" (
+    "title_id" VARCHAR   NOT NULL,
+    "title" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+CREATE TABLE "salaries" (
+    "emp_num" INT   NOT NULL,
+    "salary" INT   NOT NULL,
+    CONSTRAINT "pk_salaries" PRIMARY KEY (
+        "emp_num"
+     )
+);
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_num" FOREIGN KEY("dept_num")
+REFERENCES "departments" ("dept_num");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_num" FOREIGN KEY("emp_num")
+REFERENCES "employees" ("emp_num");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_num" FOREIGN KEY("emp_num")
+REFERENCES "employees" ("emp_num");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_num" FOREIGN KEY("dept_num")
+REFERENCES "departments" ("dept_num");
+
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "titles" ("title_id");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_num" FOREIGN KEY("emp_num")
+REFERENCES "employees" ("emp_num");
+
